734d46ab6675ff944ee4c6edfb15549c1608f460
derrrick william <williamderrick100@gmail.com>
2019-01-29 09:59:51 +0300

    Edited the user methods so as to have the save and delete method.

--- user.py
+++ user.py
@@ -6,12 +6,11 @@ class User:
 
 user_list = [] # Empty user list
 
-def __init__(self,user_name,email,password):
+def __init__(self,user_name,password):
 
       # docstring removed for simplicity
 
         self.user_name = user_name
-        self.email = email
         self.password = password
 
   user_list = [] # Empty contact list
@@ -33,45 +32,4 @@ def __init__(self,user_name,email,password):
 
         User.user_list.remove(self)
     
-    @classmethod
-    def find_by_number(cls,number):
-        '''
-        Method that takes in a number and returns a contact that matches that number.
-
-        Args:
-            number: Phone number to search for
-        Returns :
-            Contact of person that matches the number.
-        '''
-
-        for contact in cls.contact_list:
-            if contact.phone_number == number:
-                return contact
     
-    @classmethod
-    def contact_exist(cls,number):
-        '''
-        Method that checks if a contact exists from the contact list.
-        Args:
-            number: Phone number to search if it exists
-        Returns :
-            Boolean: True or false depending if the contact exists
-        '''
-        for contact in cls.contact_list:
-            if contact.phone_number == number:
-                    return True
-
-        return False
-
-    @classmethod
-    def display_contacts(cls):
-        '''
-        method that returns the contact list
-        '''
-        return cls.contact_list
-
-    @classmethod
-    def copy_email(cls,number):
-        contact_found = Contact.find_by_number(number)
-        pyperclip.copy(contact_found.email)
-        
